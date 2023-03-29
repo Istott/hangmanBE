c267166883d0a6303e51be5b878653bf86c871b3
istott <isaac.cloyd@gmail.com>
2023-03-28 22:54:24 -0600

    code cleanup

--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Express, NextFunction, Request, Response } from "express";
-import { getBookHandler } from "./controllers/books.controller";
-
-function routes(app: Express) {
-  app.get("/api/books/:bookId/:authorId/:date", getBookHandler);
-}
-
-export default routes;
